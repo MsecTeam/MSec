4f8e3aeb8b428d86228174dd673cb8deab2ff220
Msec <121532855+MsecTeam@users.noreply.github.com>
2022-12-28 17:26:20 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,24 @@
+apt install php -y
+apt install python -y
+apt install python2 -y
+apt install python3 -y
+apt install nano -y
+apt install toilet -y
+apt install figlet -y
+apt install wget -y
+apt install wcalc -y 
+apt install nmap -y
+apt install curl -y
+apt install bmon -y
+apt install apache2 -y
+apt install ruby -y
+apt install tor -y
+apt install openssl -y
+apt install macchanger -y
+apt install perl -y
+apt install unzip -y
+pip install -r requirements.txt
+echo "\033[1;34mRunning Tools\33[0m"
+sleep 2
+clear
+python2 msectools.py
